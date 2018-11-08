d2927986e080c03a0a9edfe65d659cdb38f48c49
Ayush Zenith <ayushp.zenith@gmail.com>
2018-11-08 13:00:13 -0500

    url reader now working

--- src/URLreader.java
+++ src/URLreader.java
@@ -2,15 +2,26 @@ import java.net.*;
 import java.io.*;
 
 public class URLreader {
-    public static void main(String[] args) throws Exception {
-
-        URL oracle = new URL("http://www.google.com/");
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(oracle.openStream()));
-
+    private URL URL;
+    private String HTML;
+    public URLreader(URL url) throws IOException {
+        this.URL=url;
+    }
+    public String getHTML() throws IOException {
         String inputLine;
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
+        BufferedReader in = new BufferedReader(new InputStreamReader(this.URL.openStream()));
+
+        while ((inputLine = in.readLine()) != null){
+            HTML=HTML+inputLine;
+        }
         in.close();
+        return HTML;
+    }
+
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://www.google.com/");
+        URLreader test = new URLreader(url);
+        System.out.println(test.getHTML());
+
     }
 }
