c92ec69403f6788f17dadfaeb8280e456788c4c1
Ayush Zenith <ayushp.zenith@gmail.com>
2018-11-09 09:45:14 -0500

    get and set for url

--- src/URLreader.java
+++ src/URLreader.java
@@ -4,9 +4,19 @@ import java.io.*;
 public class URLreader {
     private URL URL;
     private String HTML;
+
     public URLreader(URL url) throws IOException {
         this.URL=url;
     }
+
+    public java.net.URL getURL() {
+        return URL;
+    }
+
+    public void setURL(java.net.URL URL) {
+        this.URL = URL;
+    }
+
     public String getHTML() throws IOException {
         String inputLine;
         BufferedReader in = new BufferedReader(new InputStreamReader(this.URL.openStream()));
@@ -18,6 +28,7 @@ public class URLreader {
         return HTML;
     }
 
+    //tester
     public static void main(String[] args) throws IOException {
         URL url = new URL("https://www.google.com/");
         URLreader test = new URLreader(url);
