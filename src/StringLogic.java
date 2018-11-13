8f11c79186fac29e8e1fc21484d0beeb46b35d35
Ayush Zenith <ayushp.zenith@gmail.com>
2018-11-13 11:06:53 -0500

    getCID working

--- src/StringLogic.java
+++ src/StringLogic.java
@@ -1,3 +1,6 @@
+import java.net.*;
+import java.io.*;
+
 /**
  * Created by IntelliJ IDEA.
  *
@@ -11,6 +14,40 @@ public class StringLogic {
     private String elementName;
     private String molecularFormula;
     private String molecularWeight;
-    private String CID;
+    private int CID;
+
+    public StringLogic() {
+    }
+    public int getCID(int cid) throws IOException {
+        String string = "https://pubchem.ncbi.nlm.nih.gov/compound/"+cid;
+        URL url = new URL(string);
+        URLreader urLreader = new URLreader(url);
+
+        int cidPlacement = urLreader.getHTML().indexOf("CID");
+        int cidPlacementEnd = urLreader.getHTML().indexOf(" ",cidPlacement+4);
+        String stringCid = urLreader.getHTML().substring(cidPlacement+3,cidPlacementEnd).trim();
+        this.CID = Integer.parseInt(stringCid);
+        return CID;
+    }
+    public int getCID(String elementName) throws IOException {
+        if (elementName.indexOf(' ')!=-1){
+            elementName=elementName.replaceAll(" ", "_");
+        }
+
+        String string = "https://pubchem.ncbi.nlm.nih.gov/compound/"+elementName;
+        URL url = new URL(string);
+        URLreader urLreader = new URLreader(url);
+
+        int cidPlacement = urLreader.getHTML().indexOf("CID");
+        int cidPlacementEnd = urLreader.getHTML().indexOf(" ",cidPlacement+4);
+        String stringCid = urLreader.getHTML().substring(cidPlacement+3,cidPlacementEnd).trim();
+        this.CID = Integer.parseInt(stringCid);
+        return CID;
+    }
+    //tester
+    public static void main(String[] args) throws IOException {
+        StringLogic test = new StringLogic();
+        System.out.println(test.getCID("2,2,4-Trimethylpentane"));
+    }
 
 }
